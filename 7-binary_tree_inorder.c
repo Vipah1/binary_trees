e19405b175b74412d656d694dc5c5abb2c543148
Osman <oselibas@gmail.com>
2023-11-01 14:29:40 -0700

    Eight commit

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+* binary_tree_inorder - function scans a binary tree using in-order traversal
+* @tree: pointer to the root node of the tree to be traversed
+* @func: pointer to a function to print node data
+*/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
