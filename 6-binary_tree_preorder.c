b048d348b8e37cc18013b08daeed6d49dcd66ebc
Osman <oselibas@gmail.com>
2023-11-01 14:26:52 -0700

    Seventh commit

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+* binary_tree_preorder - func scans a binary tree using pre-order traversal
+* @tree: pointer to the root node of the tree to be traversed
+* @func: pointer to a function to print data in each node
+*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
